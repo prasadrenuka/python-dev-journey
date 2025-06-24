d0a16bf4722442fec27b2d73a88feb93e548bb57
prasadrenuka <renukaprasad2013@gmail.com>
2025-06-24 22:45:41 +0530

    co

--- day1/dataTypes.py
+++ day1/dataTypes.py
@@ -4,9 +4,9 @@ name3 = """renuka
 prasad s""" #multiline
 age = 27 # int
 is_coding = True
-complex = 2 + 3j # complex
+complexx = 2 + 3j # complex
 salary = 27000.67 # float
-print(name)
+print(name);
 print(name2)
 print(name3)
 print(age)
@@ -15,6 +15,6 @@ print(complex)
 print(type(name))
 print(type(age))
 print(type(salary))
-print(type(complex))
+print(type(complexx))
 print(type(is_coding))
 print(is_coding)
